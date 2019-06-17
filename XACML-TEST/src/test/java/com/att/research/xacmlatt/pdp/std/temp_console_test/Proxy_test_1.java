c9f7966b015903e41e81a385617c97cbcdeb87d5
dariofad <dario.fad@gmail.com>
2019-06-17 17:34:46 +0200

    add first test attempts

---
+++ XACML-TEST/src/test/java/com/att/research/xacmlatt/pdp/std/temp_console_test/Proxy_test_1.java
@@ -0,0 +1,41 @@
+package com.att.research.xacmlatt.pdp.std.temp_console_test;
+
+import com.att.research.xacml.util.FactoryException;
+import com.att.research.xacmlatt.pdp.test.TestBase;
+import com.att.research.xacmlatt.pdp.test.annotations.TestAnnotation;
+import org.apache.commons.cli.ParseException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class Proxy_test_1 {
+
+    @Test
+    public void proxy_test() {
+
+        //init args as asked by test
+        String args []  = new String[2];
+        args[0] ="-dir";
+        args[1] ="./src/test/resources/testsets/pip/configurable-csv";
+
+        //get current dir to detect broken paths
+        System.out.println("\n[+][+][+][+][+]The current working directory is " + System.getProperty("user.dir") + "[+][+][+][+][+]\n");
+
+        //do test stuff
+        try {
+
+            new TestAnnotation(args).run();
+
+        } catch (ParseException | IOException | FactoryException e) {
+            System.out.println(e.getMessage());
+        } catch (TestBase.HelpException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\n[+][+][+][+][+]test executed[+][+][+][+][+]\n");
+
+        return ;
+
+    }
+
+}
---
+++ XACML-TEST/src/test/java/com/att/research/xacmlatt/pdp/std/temp_console_test/Proxy_test_2.java
@@ -0,0 +1,46 @@
+package com.att.research.xacmlatt.pdp.std.temp_console_test;
+
+import com.att.research.xacml.util.FactoryException;
+import com.att.research.xacmlatt.pdp.test.TestBase;
+import com.att.research.xacmlatt.pdp.test.policy.TestPolicy;
+import org.apache.commons.cli.ParseException;
+import org.junit.Test;
+import java.io.IOException;
+
+public class Proxy_test_2 {
+
+    @Test
+    public void proxy_test() {
+
+        //TODO understand if everything is really working
+
+        // to compile new tests inside this module please type
+        // mvn package -DskipTests
+        // to run this test position inside the directory of XACML-TEST module and run the following command
+        // mvn -Dtest=Proxy_test_2 -Dxacml.properties=src/test/resources/testsets/policy/xacml_mod.properties test
+
+        //init args as asked by test
+        String args []  = new String[2];
+        args[0] ="-dir";
+        args[1] ="./src/test/resources/testsets/pip/configurable-csv";
+
+        //get current dir to detect broken paths
+        System.out.println("\n[+][+][+][+][+]The current working directory is " + System.getProperty("user.dir") + "[+][+][+][+][+]\n");
+
+        //do test stuff
+        try {
+
+            new TestPolicy(args).run();
+
+        } catch (ParseException | IOException | FactoryException e) {
+            System.out.println(e.getMessage());
+        } catch (TestBase.HelpException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\n[+][+][+][+][+]test executed[+][+][+][+][+]\n");
+
+        return ;
+    }
+
+}
